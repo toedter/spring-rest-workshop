d3f0ba2995218cb59b2377fc218417d604aa3ff2
Kai Toedter <kai@toedter.com>
2022-03-06 20:08:28 +0100

    fix: fix lab 2 and 3 tests

--- lab2/complete/src/test/java/com/toedter/workshops/springrest/lab2/movie/MovieResourceIntegrationTest.java
+++ lab2/complete/src/test/java/com/toedter/workshops/springrest/lab2/movie/MovieResourceIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
+import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -38,21 +39,20 @@ public class MovieResourceIntegrationTest {
     }
 
     @Test
-    public void shouldGetUsersLink() throws Exception {
-        MockHttpServletResponse response2 = mvc.perform(get("/")).
+    public void shouldGetMovies() throws Exception {
+        MockHttpServletResponse response2 = mvc.perform(get("/api/movies")).
                 andDo(MockMvcResultHandlers.print()).
                 andExpect(status().isOk()).
-                andExpect(content().contentType("application/hal+json")).
-                andExpect(jsonPath("_links.movies.href", CoreMatchers.notNullValue())).
+                andExpect(content().contentType(HAL_JSON_VALUE)).
+                andExpect(jsonPath("_embedded.movies", CoreMatchers.notNullValue())).
                 andReturn().
                 getResponse();
 
         LinkDiscoverer discoverer = links.getLinkDiscovererFor(response2.getContentType()).get();
-        Link link = discoverer.findLinkWithRel("movies", response2.getContentAsString()).get();
+        Link link = discoverer.findLinkWithRel("self", response2.getContentAsString()).get();
         String href = link.getHref();
-        String hrefWithoutTemplateParameters = href.substring(0, href.indexOf("{"));
 
-        mvc.perform(get(hrefWithoutTemplateParameters)).
+        mvc.perform(get(href)).
                 andDo(MockMvcResultHandlers.print()).
                 andExpect(content().contentTypeCompatibleWith(MediaTypes.HAL_JSON)).
                 andExpect(jsonPath("_embedded.movies", CoreMatchers.notNullValue()));
