13dfd9083a8ebe40b4bd942dc516183e51e8a34f
Kai Toedter <kai@toedter.com>
2023-11-09 07:55:58 +0100

    chore: update to Spring Boot 3.1.5

--- lab2/complete/src/main/java/com/toedter/workshops/springrest/lab2/movie/MovieRepository.java
+++ lab2/complete/src/main/java/com/toedter/workshops/springrest/lab2/movie/MovieRepository.java
@@ -1,7 +1,8 @@
 package com.toedter.workshops.springrest.lab2.movie;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface MovieRepository extends CrudRepository<Movie, Long>, PagingAndSortingRepository<Movie, Long> {
+public interface MovieRepository extends JpaRepository<Movie, Long> {
 }
