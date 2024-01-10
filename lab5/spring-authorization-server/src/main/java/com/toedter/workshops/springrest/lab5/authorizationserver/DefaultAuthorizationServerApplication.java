70ff0e3c6de65deec3e34c5494b2396e2f75f06d
Kai Toedter <kai@toedter.com>
2024-01-10 15:25:55 +0100

    feat: add Spring Authorization Server to lab 5

---
+++ lab5/spring-authorization-server/src/main/java/com/toedter/workshops/springrest/lab5/authorizationserver/DefaultAuthorizationServerApplication.java
@@ -0,0 +1,44 @@
+package com.toedter.workshops.springrest.lab5.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenClaimsContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class DefaultAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultAuthorizationServerApplication.class, args);
+	}
+
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager(SecurityProperties properties) {
+		SecurityProperties.User user = properties.getUser();
+		List<String> roles = user.getRoles();
+		return new InMemoryUserDetailsManager(User.withUsername(user.getName())
+				.password(user.getPassword())
+				.roles(StringUtils.toStringArray(roles))
+				.build());
+	}
+
+	@Bean
+	public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+		return context -> {
+			context.getClaims().audience(Collections.singletonList("movies-audience"));
+		};
+	}
+}
