bbc4fb84b2f82b03e74bbced764e71dd9968a705
Kai Toedter <kai@toedter.com>
2024-03-10 12:25:39 +0100

    chore: add security labs

--- lab5/initial/src/main/java/com/toedter/workshops/springrest/lab4/movie/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.toedter.workshops.springrest.lab4.movie;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface MovieRepository extends CrudRepository<Movie, Long>, PagingAndSortingRepository<Movie, Long> {
-}
---
+++ lab5/initial/src/main/java/com/toedter/workshops/springrest/lab5/security/OAuth2ResourceServerSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.toedter.workshops.springrest.lab5.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class OAuth2ResourceServerSecurityConfiguration {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    String jwkSetUri;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // add security configuration
+        return http.build();
+    }
+
+}
